8a297e020ac134210b934c113ec393ab0db3e076
Carlos Mendez <calicci.gardachi@gmail.com>
2024-05-28 19:35:34 -1000

    Create rocm_notes.sh
    Output of queries regarding ROCm installation.

---
+++ scripts/ollama/rocm_notes.sh
@@ -0,0 +1,275 @@
+rocminfo
+ROCk module version 6.7.0 is loaded
+=====================    
+HSA System Attributes    
+=====================    
+Runtime Version:         1.13
+Runtime Ext Version:     1.4
+System Timestamp Freq.:  1000.000000MHz
+Sig. Max Wait Duration:  18446744073709551615 (0xFFFFFFFFFFFFFFFF) (timestamp count)
+Machine Model:           LARGE                              
+System Endianness:       LITTLE                             
+Mwaitx:                  DISABLED
+DMAbuf Support:          YES
+
+==========               
+HSA Agents               
+==========               
+*******                  
+Agent 1                  
+*******                  
+  Name:                    Common KVM processor               
+  Uuid:                    CPU-XX                             
+  Marketing Name:          Common KVM processor               
+  Vendor Name:             CPU                                
+  Feature:                 None specified                     
+  Profile:                 FULL_PROFILE                       
+  Float Round Mode:        NEAR                               
+  Max Queue Number:        0(0x0)                             
+  Queue Min Size:          0(0x0)                             
+  Queue Max Size:          0(0x0)                             
+  Queue Type:              MULTI                              
+  Node:                    0                                  
+  Device Type:             CPU                                
+  Cache Info:              
+    L1:                      32768(0x8000) KB                   
+  Chip ID:                 0(0x0)                             
+  ASIC Revision:           0(0x0)                             
+  Cacheline Size:          64(0x40)                           
+  Max Clock Freq. (MHz):   0                                  
+  BDFID:                   0                                  
+  Internal Node ID:        0                                  
+  Compute Unit:            4                                  
+  SIMDs per CU:            0                                  
+  Shader Engines:          0                                  
+  Shader Arrs. per Eng.:   0                                  
+  WatchPts on Addr. Ranges:1                                  
+  Features:                None
+  Pool Info:               
+    Pool 1                   
+      Segment:                 GLOBAL; FLAGS: FINE GRAINED        
+      Size:                    12238688(0xbabf60) KB              
+      Allocatable:             TRUE                               
+      Alloc Granule:           4KB                                
+      Alloc Recommended Granule:4KB                                
+      Alloc Alignment:         4KB                                
+      Accessible by all:       TRUE                               
+    Pool 2                   
+      Segment:                 GLOBAL; FLAGS: KERNARG, FINE GRAINED
+      Size:                    12238688(0xbabf60) KB              
+      Allocatable:             TRUE                               
+      Alloc Granule:           4KB                                
+      Alloc Recommended Granule:4KB                                
+      Alloc Alignment:         4KB                                
+      Accessible by all:       TRUE                               
+    Pool 3                   
+      Segment:                 GLOBAL; FLAGS: COARSE GRAINED      
+      Size:                    12238688(0xbabf60) KB              
+      Allocatable:             TRUE                               
+      Alloc Granule:           4KB                                
+      Alloc Recommended Granule:4KB                                
+      Alloc Alignment:         4KB                                
+      Accessible by all:       TRUE                               
+  ISA Info:                
+*******                  
+Agent 2                  
+*******                  
+  Name:                    gfx1100                            
+  Uuid:                    GPU-cf769582795e8164               
+  Marketing Name:          Radeon RX 7900 XT                  
+  Vendor Name:             AMD                                
+  Feature:                 KERNEL_DISPATCH                    
+  Profile:                 BASE_PROFILE                       
+  Float Round Mode:        NEAR                               
+  Max Queue Number:        128(0x80)                          
+  Queue Min Size:          64(0x40)                           
+  Queue Max Size:          131072(0x20000)                    
+  Queue Type:              MULTI                              
+  Node:                    1                                  
+  Device Type:             GPU                                
+  Cache Info:              
+    L1:                      32(0x20) KB                        
+    L2:                      6144(0x1800) KB                    
+    L3:                      81920(0x14000) KB                  
+  Chip ID:                 29772(0x744c)                      
+  ASIC Revision:           0(0x0)                             
+  Cacheline Size:          64(0x40)                           
+  Max Clock Freq. (MHz):   2219                               
+  BDFID:                   128                                
+  Internal Node ID:        1                                  
+  Compute Unit:            84                                 
+  SIMDs per CU:            2                                  
+  Shader Engines:          6                                  
+  Shader Arrs. per Eng.:   2                                  
+  WatchPts on Addr. Ranges:4                                  
+  Coherent Host Access:    FALSE                              
+  Features:                KERNEL_DISPATCH 
+  Fast F16 Operation:      TRUE                               
+  Wavefront Size:          32(0x20)                           
+  Workgroup Max Size:      1024(0x400)                        
+  Workgroup Max Size per Dimension:
+    x                        1024(0x400)                        
+    y                        1024(0x400)                        
+    z                        1024(0x400)                        
+  Max Waves Per CU:        32(0x20)                           
+  Max Work-item Per CU:    1024(0x400)                        
+  Grid Max Size:           4294967295(0xffffffff)             
+  Grid Max Size per Dimension:
+    x                        4294967295(0xffffffff)             
+    y                        4294967295(0xffffffff)             
+    z                        4294967295(0xffffffff)             
+  Max fbarriers/Workgrp:   32                                 
+  Packet Processor uCode:: 92                                 
+  SDMA engine uCode::      20                                 
+  IOMMU Support::          None                               
+  Pool Info:               
+    Pool 1                   
+      Segment:                 GLOBAL; FLAGS: COARSE GRAINED      
+      Size:                    20955136(0x13fc000) KB             
+      Allocatable:             TRUE                               
+      Alloc Granule:           4KB                                
+      Alloc Recommended Granule:2048KB                             
+      Alloc Alignment:         4KB                                
+      Accessible by all:       FALSE                              
+    Pool 2                   
+      Segment:                 GLOBAL; FLAGS: EXTENDED FINE GRAINED
+      Size:                    20955136(0x13fc000) KB             
+      Allocatable:             TRUE                               
+      Alloc Granule:           4KB                                
+      Alloc Recommended Granule:2048KB                             
+      Alloc Alignment:         4KB                                
+      Accessible by all:       FALSE                              
+    Pool 3                   
+      Segment:                 GROUP                              
+      Size:                    64(0x40) KB                        
+      Allocatable:             FALSE                              
+      Alloc Granule:           0KB                                
+      Alloc Recommended Granule:0KB                                
+      Alloc Alignment:         0KB                                
+      Accessible by all:       FALSE                              
+  ISA Info:                
+    ISA 1                    
+      Name:                    amdgcn-amd-amdhsa--gfx1100         
+      Machine Models:          HSA_MACHINE_MODEL_LARGE            
+      Profiles:                HSA_PROFILE_BASE                   
+      Default Rounding Mode:   NEAR                               
+      Default Rounding Mode:   NEAR                               
+      Fast f16:                TRUE                               
+      Workgroup Max Size:      1024(0x400)                        
+      Workgroup Max Size per Dimension:
+        x                        1024(0x400)                        
+        y                        1024(0x400)                        
+        z                        1024(0x400)                        
+      Grid Max Size:           4294967295(0xffffffff)             
+      Grid Max Size per Dimension:
+        x                        4294967295(0xffffffff)             
+        y                        4294967295(0xffffffff)             
+        z                        4294967295(0xffffffff)             
+      FBarrier Max Size:       32                                 
+*** Done ***
+             
+ubuntu@ollama:~$ sudo clinfo
+Number of platforms:				 1
+  Platform Profile:				 FULL_PROFILE
+  Platform Version:				 OpenCL 2.1 AMD-APP (3614.0)
+  Platform Name:				 AMD Accelerated Parallel Processing
+  Platform Vendor:				 Advanced Micro Devices, Inc.
+  Platform Extensions:				 cl_khr_icd cl_amd_event_callback 
+
+
+  Platform Name:				 AMD Accelerated Parallel Processing
+Number of devices:				 1
+  Device Type:					 CL_DEVICE_TYPE_GPU
+  Vendor ID:					 1002h
+  Board name:					 Radeon RX 7900 XT
+  Device Topology:				 PCI[ B#0, D#16, F#0 ]
+  Max compute units:				 42
+  Max work items dimensions:			 3
+    Max work items[0]:				 1024
+    Max work items[1]:				 1024
+    Max work items[2]:				 1024
+  Max work group size:				 256
+  Preferred vector width char:			 4
+  Preferred vector width short:			 2
+  Preferred vector width int:			 1
+  Preferred vector width long:			 1
+  Preferred vector width float:			 1
+  Preferred vector width double:		 1
+  Native vector width char:			 4
+  Native vector width short:			 2
+  Native vector width int:			 1
+  Native vector width long:			 1
+  Native vector width float:			 1
+  Native vector width double:			 1
+  Max clock frequency:				 2219Mhz
+  Address bits:					 64
+  Max memory allocation:			 18239350368
+  Image support:				 Yes
+  Max number of images read arguments:		 128
+  Max number of images write arguments:		 8
+  Max image 2D width:				 16384
+  Max image 2D height:				 16384
+  Max image 3D width:				 16384
+  Max image 3D height:				 16384
+  Max image 3D depth:				 8192
+  Max samplers within kernel:			 16
+  Max size of kernel argument:			 1024
+  Alignment (bits) of base address:		 1024
+  Minimum alignment (bytes) for any datatype:	 128
+  Single precision floating point capability
+    Denorms:					 Yes
+    Quiet NaNs:					 Yes
+    Round to nearest even:			 Yes
+    Round to zero:				 Yes
+    Round to +ve and infinity:			 Yes
+    IEEE754-2008 fused multiply-add:		 Yes
+  Cache type:					 Read/Write
+  Cache line size:				 64
+  Cache size:					 32768
+  Global memory size:				 21458059264
+  Constant buffer size:				 18239350368
+  Max number of constant args:			 8
+  Local memory type:				 Scratchpad
+  Local memory size:				 65536
+  Max pipe arguments:				 16
+  Max pipe active reservations:			 16
+  Max pipe packet size:				 1059481184
+  Max global variable size:			 18239350368
+  Max global variable preferred total size:	 21458059264
+  Max read/write image args:			 64
+  Max on device events:				 1024
+  Queue on device max size:			 8388608
+  Max on device queues:				 1
+  Queue on device preferred size:		 262144
+  SVM capabilities:				 
+    Coarse grain buffer:			 Yes
+    Fine grain buffer:				 Yes
+    Fine grain system:				 No
+    Atomics:					 No
+  Preferred platform atomic alignment:		 0
+  Preferred global atomic alignment:		 0
+  Preferred local atomic alignment:		 0
+  Kernel Preferred work group size multiple:	 32
+  Error correction support:			 0
+  Unified memory for Host and Device:		 0
+  Profiling timer resolution:			 1
+  Device endianess:				 Little
+  Available:					 Yes
+  Compiler available:				 Yes
+  Execution capabilities:				 
+    Execute OpenCL kernels:			 Yes
+    Execute native function:			 No
+  Queue on Host properties:				 
+    Out-of-Order:				 No
+    Profiling :					 Yes
+  Queue on Device properties:				 
+    Out-of-Order:				 Yes
+    Profiling :					 Yes
+  Platform ID:					 0x7faeea2e4fd0
+  Name:						 gfx1100
+  Vendor:					 Advanced Micro Devices, Inc.
+  Device OpenCL C version:			 OpenCL C 2.0 
+  Driver version:				 3614.0 (HSA1.1,LC)
+  Profile:					 FULL_PROFILE
+  Version:					 OpenCL 2.0 
+  Extensions:					 cl_khr_fp64 cl_khr_global_int32_base_atomics cl_khr_global_int32_extended_atomics cl_khr_local_int32_base_atomics cl_khr_local_int32_extended_atomics cl_khr_int64_base_atomics cl_khr_int64_extended_atomics cl_khr_3d_image_writes cl_khr_byte_addressable_store cl_khr_fp16 cl_khr_gl_sharing cl_amd_device_attribute_query cl_amd_media_ops cl_amd_media_ops2 cl_khr_image2d_from_buffer cl_khr_subgroups cl_khr_depth_images cl_amd_copy_buffer_p2p cl_amd_assembly_program 
