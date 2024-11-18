772bd34e8afba3370c7b991d119566c1961f4002
Carlos Mendez <calicci.gardachi@gmail.com>
2024-11-18 09:18:09 -1000

    Update ollama_docker.sh
    Update during testing.

--- scripts/ollama/ollama_docker.sh
+++ scripts/ollama/ollama_docker.sh
@@ -3,12 +3,12 @@
 # * /******************************************************************/
 # * Copyright         : <2024> <Everybody>
 # * File Name         : <ollama_docker.sh>
-# * Description       : <Setup Ollama and OpenWeb UI>
+# * Description       : <Setup Ollama, ROCm, and OpenWeb UI>
 # *                    
 # * Revision History  :
 # * Date		Author 			Comments
 # * ------------------------------------------------------------------
-# * 05/11/2024	<iCarlosMendez>	<Insprired by ChatGPT>
+# * 11/17/2024	<iCarlosMendez>	<Insprired by ChatGPT>
 # *
 # * /******************************************************************/
 
@@ -20,9 +20,13 @@
 # Prerequisites: Download and run the following script.
 # wget https://raw.githubusercontent.com/icarlosmendez/admin_automation/master/scripts/ollama/ollama_docker.sh
 
-# Barebones Ollama on linux
+
+# ##################################################### #
+# Install barebones Ollama on Linux
 # curl -fsSL https://ollama.com/install.sh | sh
 
+
+# ##################################################### #
 # Prep the dist for package install
 # Add Docker's official GPG key:
 sudo apt-get update
@@ -31,6 +35,8 @@ sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+
+# ##################################################### #
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
@@ -38,25 +44,39 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
+
+# ##################################################### #
 # Install Docker in preparation for the next step.
 # Install "all" the Docker packages.
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Open WebUI running alone in a Docker container.
+# Install Open WebUI running alone in a Docker container.
+# docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
 # Used when Ollama is installed on the Docker host.
-docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
 
-# Or this alternative command when you have issues connecting to ollama
-# docker run -d --network=host -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://127.0.0.1:11434 --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+# Or this alternative when you have issues connecting to ollama, which I seem to. I use this one.
+docker run -d --network=host -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://127.0.0.1:11434 --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+# Used when Ollama is installed on the Docker host.
+
+
+# ##################################################### #
+# Pull 22.04 image from Docker Hub
+sudo docker pull rocm/dev-ubuntu-22.04:6.2.4-complete
+
+# Install ROCm running alone in a container.
+# Paired with 22.04 because that's the image I pulled from Docker Hub 
+sudo docker run --device /dev/kfd --device /dev/dri --security-opt seccomp=unconfined rocm/dev-ubuntu-22.04:6.2.4-complete
 
 # Open WebUI bundled with Ollama in a single Docker container. Accoring to my testing, this doesn't work with AMD GPUs.
 # docker run --device /dev/kfd --device /dev/dri --security-opt seccomp=unconfined -d -p 3000:8080 --gpus=all -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
 
+
+# ##################################################### #
 # First, create the volume that Portainer Server will use to store its database:
-docker volume create portainer_data
+sudo docker volume create portainer_data
 
 # Then, download and install the Portainer Server container:
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 
 
